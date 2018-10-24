473af9eee1a186a66815f8f26c366ac719313190
jmbugajski <39573387+jmbugajski@users.noreply.github.com>
2018-10-23 23:49:03 -0700

    working initial version

---
+++ js/jquery-select.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+	// Reset Search Form
+	$('#btn-clear').click(function() {
+		$('#form-search')[0].reset();
+			return false;
+	});
+
+	// Grab Text from Search Box and Encode It
+	$('#btn-run').click(function () {
+		var urlPart1 = 'https://googlecloud.looker.com/embed/dashboards/226?Log%20Search=%22'
+		var urlPart2 = '%22&filter_config=%7B%22Log%20Search%22:%5B%7B%22type%22:%22%3D%22,%22values%22:%5B%7B%22constant%22:%22'
+		var urlPart3 = '%22%7D,%7B%7D%5D,%22id%22:1%7D%5D%7D'
+		var textInput = $("#search-box").val();
+		var encodedUrl = encodeURIComponent(textInput);
+		var urlFinal = urlPart1 + encodedUrl + urlPart2 + encodedUrl + urlPart3
+		$('#looker-frame').attr('src', urlFinal);
+		return false; 
+	});
+
+	// Change Overview Button URL when Test Store Group Selected
+	$('[name="store-11"]').change(function() {
+		if($(this).is(":checked")) {
+			$("#product-and-store").html("<a target='iframe_a' href='https://35.227.249.152/embed/dashboards/24'><span>By Product & Store</span></a>");
+		}
+		if(!$(this).is(':checked')) {
+			$("#product-and-store").html("<a target='iframe_a' href='https://35.227.249.152/embed/dashboards/25'><span>By Product & Store</span></a>");
+		}
+	});
+
+});
