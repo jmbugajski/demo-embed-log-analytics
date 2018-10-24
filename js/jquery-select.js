7a1f97bd2b8115230a1e366d64a4dd8f5b340de9
jmbugajski <39573387+jmbugajski@users.noreply.github.com>
2018-10-24 10:53:29 -0700

    add string repair to jquery and fix weird css issue

--- js/jquery-select.js
+++ js/jquery-select.js
@@ -11,7 +11,9 @@ $(document).ready(function () {
 		var urlPart2 = '%22&filter_config=%7B%22Log%20Search%22:%5B%7B%22type%22:%22%3D%22,%22values%22:%5B%7B%22constant%22:%22'
 		var urlPart3 = '%22%7D,%7B%7D%5D,%22id%22:1%7D%5D%7D'
 		var textInput = $("#search-box").val();
-		var encodedUrl = encodeURIComponent(textInput);
+		var textInputEsc1 = textInput.replace(/\\/g,"\\\\");
+		var textInputEsc2 = textInputEsc1.replace(/"/g, '\\"');
+		var encodedUrl = encodeURIComponent(textInputEsc2);
 		var urlFinal = urlPart1 + encodedUrl + urlPart2 + encodedUrl + urlPart3
 		$('#looker-frame').attr('src', urlFinal);
 			return false; 
